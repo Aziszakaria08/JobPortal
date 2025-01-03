2603357b6e84b28809d17855d21b1b5a1d896763
Aziszakaria08 <azisspoy08@gmail.com>
2025-01-03 21:26:34 +0700

    Add project files.

---
+++ JobPortal/User/Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JobPortal.User
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        SqlConnection cdm;
+        SqlCommand cmd;
+        string str = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        //protected void btnSend_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+
+        //    }
+        //    catch
+
+        //}
+    }
+}
